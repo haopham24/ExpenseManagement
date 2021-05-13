576ba2cf3f7cfc1454ae646e7ef1bbb9fc3cac2f
haopham24 <haoptt.244@gmail.com>
2021-05-13 10:39:01 +0700

    add project

---
+++ src/expensemanagement/Money.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expensemanagement;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author huyen
+ */
+public class Money implements Serializable{
+    
+    private Date date;
+    private String category, note;
+    private int money;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+    
+    
+    
+}
